a56f3bc61a6a1034d3e51ed04043cede423e5b40
jaycorpstudios <jaycorpstudios@me.com>
2019-01-09 22:21:23 -0600

    JS Strings

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ var name = 'Lord Jay';
 //ES6
 let age = 35;
 const birthday = '23/10/2000';
+
+
+//Concatenar cadenas
+console.log('Hello world!!! ' + name);
+
+//String Interpolation
+console.log(`Mi cumpleaños es el ${birthday} para que me feliciten y así `)
+console.log(`Y 5 + 10 es: ${5 + 10}`);
